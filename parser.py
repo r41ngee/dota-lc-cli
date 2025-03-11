66aaa4081d62548c0405331b96a06a8e5873447c
r41ngee <r41ngee@yandex.ru>
2025-03-11 23:25:01 +0300

    0.1.0

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import json
+import os
+# import pathlib
 
-def parse_localization_string(localization_string):
+def parse_lstr(localization_string):
     """
     Парсит строку локализации в словарь Python.
 
@@ -40,7 +42,40 @@ def parse_localization_string(localization_string):
 
     return result
 
+def generate_lstr(localization_dict):
+    """
+    Генерирует строку локализации из словаря Python.
+
+    Args:
+        localization_dict: Словарь Python, представляющий структуру локализации.
+
+    Returns:
+        Строка локализации в исходном формате.
+    """
+    output_string = ""
+
+    if 'lang' in localization_dict:
+        output_string += '"lang"\n{\n'
+        lang_section = localization_dict['lang']
+
+        if 'Language' in lang_section:
+            output_string += f'\t"Language"\t\t"{lang_section["Language"]}"\n'
+
+        if 'Tokens' in lang_section:
+            output_string += '\t"Tokens"\n\t{\n'
+            tokens_section = lang_section['Tokens']
+            for token_key, token_value in tokens_section.items():
+                output_string += f'\t\t"{token_key}"\t\t"{token_value}"\n'
+            output_string += '\t}\n'
+
+        output_string += '}\n'
+
+    return output_string
+
 with open("abilities_russian.txt", encoding="utf-8") as file:
     with open("abilities_russian.json", "w", encoding="utf-8") as jfile:
-        json.dump(parse_localization_string(file.read()), jfile, ensure_ascii=False)
+        content = file.read()
+        json.dump(parse_lstr(content), jfile, ensure_ascii=False, indent=4) 
 
+
+os.execv("bin/vpk.exe", ["-help"])
