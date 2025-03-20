a04029d71df64f0e80f43aecffe26e738e82ab54
r41ngee <r41ngee@yandex.ru>
2025-03-20 18:59:05 +0300

    cleaned files and cleaned main method

--- clilogic.py
+++ clilogic.py
@@ -8,24 +8,12 @@ class Skill:
 
         self.desc = desc
 
-    def ToKeys(self) -> dict:
-        linked_keys = sum([{i: self.name} for i in self.linked])
-
-        return {
-            self.key: self.name,
-        } + linked_keys
-
 class Facet:
     def __init__(self, desc):
         self.name = desc["name"]
         self.key = desc["key"]
 
         self.desc = desc
-
-    def ToKeys(self) -> dict:
-        return {
-            self.key : self.name
-        }
 
 class Hero:
     def __init__(self, desc: dict):
@@ -36,15 +24,8 @@ class Hero:
 
         self.desc = desc
 
-    def ToKeys(self):
-        return {
-            self.key: self.name
-        } + sum([i.ToKeys() for i in self.skills]) + sum([i.ToKeys() for i in self.facets])
-
 def clear_console():
-    # Для Windows
     if os.name == 'nt':
         os.system('cls')
-    # Для Unix-подобных систем (Linux, macOS)
     else:
         os.system('clear')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import keyparser
+import localparser
+import jsontopickle
+from clilogic import *
 from tkinter.filedialog import askdirectory
 import json
 
-with open("user/settings.json") as file:
-    settings = json.load(file)
 
-if settings["dota_directory"] == None:
-    settings["dota_directory"] = askdirectory()
-    with open("user/settings.json", "w") as file:
-        json.dump(settings, file)
+def main():
+    ...
+
+if __name__=="__main__":
+    main()
