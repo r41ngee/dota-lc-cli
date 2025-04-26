76f6f27d80b759d795ced5717dfa77822cff5492
r41ngee <r41ngee@yandex.ru>
2025-04-26 18:18:31 +0300

    Удаляет неиспользуемые функции логирования
    Убраны функции keylog и endlog из файла misc.py, что упрощает код и устраняет лишние зависимости.

--- dota_localization_changer/misc.py
+++ dota_localization_changer/misc.py
@@ -2,17 +2,8 @@
 # - r41ngee -
 # -----------
 
-import logging
 import os
 
 
 def cls():
     os.system("cls")
-
-
-def keylog(key, val):
-    logging.info(f'New value for key "{key}": "{val}"')
-
-
-def endlog(code: int):
-    logging.info(f"Program exit with code {hex(code)}")
